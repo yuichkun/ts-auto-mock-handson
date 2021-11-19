0e4987b30383d2de7869ca9d9b9f62c82445fbf9
Yuichi Yogo <yuichkun@gmail.com>
2021-11-19 14:47:17 +0900

    add mock list example

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,8 @@
-import { createMock } from 'ts-auto-mock';
+import { createMock, createMockList } from 'ts-auto-mock';
+
+type Organization = {
+    people: Person[];
+}
 interface Person {
     id: string;
     getName(): string;
@@ -6,8 +10,17 @@ interface Person {
         phone: number
     }
 }
-const mock = createMock<Person>();
 
-console.log(mock.id)
-console.log(mock.getName())
-console.log(mock.details)
+const organization = createMock<Organization>(
+    {
+        people: createMockList<Person>(4)
+    }
+)
+console.log('organization', organization);
+organization.people.forEach(printPerson);
+
+function printPerson(person: Person) {
+    console.log('id', person.id);
+    console.log('name', person.getName());
+    console.log('details', person.details.phone);
+}
